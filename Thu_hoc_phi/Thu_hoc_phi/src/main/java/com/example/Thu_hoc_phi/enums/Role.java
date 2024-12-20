f3b0f130996eff9a06789bca653374f0a292e559
PhanQuangR111103 <161630245+PhanQuangR111103@users.noreply.github.com>
2024-12-20 12:26:31 +0700

    Delete Thu_hoc_phi/Thu_hoc_phi directory

--- Thu_hoc_phi/Thu_hoc_phi/src/main/java/com/example/Thu_hoc_phi/enums/Role.java
+++
@@ -1,6 +0,0 @@
-package com.example.Thu_hoc_phi.enums;
-
-public enum Role {
-    ADMIN,
-    USER
-}
--- Thu_hoc_phi/Thu_hoc_phi/src/main/java/com/example/Thu_hoc_phi/repository/SubjectRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.Thu_hoc_phi.repository;
-
-import com.example.Thu_hoc_phi.entity.Subject;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SubjectRepository extends JpaRepository<Subject, Long> {
-}
